b52f1b77261a5bcdaf641d3f9c84a355a76ceb97
Nathaniello <139955367+NathanTzy@users.noreply.github.com>
2024-04-08 10:07:36 +0700

    m

--- src/public/script.js
+++ src/public/script.js
@@ -9,3 +9,32 @@ btnReadMore.addEventListener('click', () => {
         btnReadMore.textContent = 'Read More';
     }
 });
+
+const slider = document.querySelector('.slider');
+const items = document.querySelectorAll('.item');
+const btns = document.querySelectorAll('.btn');
+
+function reset() {
+    for (let i = 0; i < items.length; i++) {
+        btns[i].classList.remove('expand');
+        items[i].classList.remove('animation');
+    }
+}
+
+function animate(i) {
+    btns[i].classList.add('expand');
+    items[i].classList.add('animation');
+}
+
+function scrollTo(i) {
+    slider.style.transform = `translateX(${-i * slider.offsetWidth}px)`;
+    reset();
+    animate(i);
+}
+
+const activate = (e) => e.target.matches('.btn') && scrollTo(e.target.dataset.index);
+
+const init = () => animate(0);
+
+window.addEventListener('load', init, false);
+window.addEventListener('click', activate, false);
